090a0ee510ff464676bcfa16879cedbac818080f
ellywynn <canttrymybest@gmail.com>
2021-11-28 18:59:22 +0300

    interfaces for repositories and services

---
+++ internal/app/models/interfaces.go
@@ -0,0 +1,36 @@
+package models
+
+type UserService interface {
+	SignUp(user *User) (int, error)
+	GetByEmail(email string) (*User, error)
+	GetById(userId int) (*User, error)
+	GetByUsername(username string) (*User, error)
+	GetAll() (*[]User, error)
+	Update(userId int) error
+	Delete(userId int) error
+}
+
+type UserRepository interface {
+	Create(user *User) (int, error)
+	FindByEmail(email string) (*User, error)
+	FindByUsername(username string) (*User, error)
+	FindById(userId int) (*User, error)
+	GetAll() (*[]User, error)
+	Update(userId int) error
+	Delete(userId int) error
+}
+
+type AuthRepository interface {
+	Login(input *AuthLoginStruct) error
+	Logout() error
+}
+
+type AuthService interface {
+	Login(input *AuthLoginStruct) error
+	Logout() error
+}
+
+type AuthLoginStruct struct {
+	Email    string
+	Password string
+}
