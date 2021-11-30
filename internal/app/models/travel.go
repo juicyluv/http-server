68fad26ff1842786958d8b3252311195c7a074dc
ellywynn <canttrymybest@gmail.com>
2021-11-30 19:53:25 +0300

    fixed update structure and removed require fild

--- internal/app/models/travel.go
+++ internal/app/models/travel.go
@@ -1,26 +1,46 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+)
 
 type Travel struct {
-	Id           uint      `json:"id"`
-	Title        string    `json:"title" binding:"required"`
-	DurationDays int       `json:"duration_days" db:"duration_days" binding:"required"`
-	Price        int       `json:"price" binding:"required"`
-	PartySize    int       `json:"party_size" db:"party_size"`
-	Complexity   int       `json:"complexity" binding:"required"`
-	Place        int       `json:"place" binding:"required"`
-	Description  string    `json:"description" binding:"required"`
-	Date         time.Time `json:"date" binding:"required"`
+	Id           uint        `json:"id"`
+	Title        string      `json:"title" binding:"required"`
+	DurationDays int         `json:"duration_days" db:"duration_days" binding:"required"`
+	Price        int         `json:"price" binding:"required"`
+	PartySize    int         `json:"party_size" db:"party_size"`
+	Complexity   int         `json:"complexity" binding:"required"`
+	Place        interface{} `json:"place" binding:"required"`
+	Description  string      `json:"description" binding:"required"`
+	Date         time.Time   `json:"date" binding:"required"`
 }
 
 type UpdateTravelInput struct {
-	Title        *string    `json:"title" binding:"required"`
-	DurationDays *int       `json:"duration_days" db:"duration_days" binding:"required"`
-	Price        *int       `json:"price" binding:"required"`
+	Title        *string    `json:"title"`
+	DurationDays *int       `json:"duration_days" db:"duration_days"`
+	Price        *int       `json:"price"`
 	PartySize    *int       `json:"party_size" db:"party_size"`
-	Complexity   *int       `json:"complexity" binding:"required"`
-	Place        *int       `json:"place" binding:"required"`
-	Description  *string    `json:"description" binding:"required"`
-	Date         *time.Time `json:"date" binding:"required"`
+	Complexity   *int       `json:"complexity"`
+	Place        *int       `json:"place"`
+	Description  *string    `json:"description"`
+	Date         *time.Time `json:"date"`
+}
+
+// Validates creating user struct
+func (t *Travel) Validate() error {
+	return validation.ValidateStruct(
+		t,
+		validation.Field(&t.Title, validation.Required),
+		validation.Field(&t.DurationDays, validation.Required),
+		validation.Field(&t.Price, validation.Required),
+		validation.Field(&t.PartySize, is.Int),
+		validation.Field(&t.Complexity, validation.Required),
+		validation.Field(&t.Description, is.ASCII, validation.Required),
+		validation.Field(&t.Date, validation.Required),
+		validation.Field(&t.Place, validation.Required),
+	)
 }
