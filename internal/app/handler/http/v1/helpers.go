0b96df0e672aad6667e250409922613ef5b57a26
ellywynn <canttrymybest@gmail.com>
2021-12-01 22:10:35 +0300

    helper function to get session user id as int

---
+++ internal/app/handler/http/v1/helpers.go
@@ -0,0 +1,22 @@
+package v1
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getSessionUserId(h *Handler, c *gin.Context) (int, error) {
+	session, _ := h.sessionStore.Get(c.Request, coockieName)
+	userId, exists := session.Values["user_id"]
+	if !exists {
+		return 0, errors.New("you need to sign in")
+	}
+
+	userIdInt, ok := userId.(int)
+	if !ok {
+		return 0, errors.New("user id should be an integer")
+	}
+
+	return userIdInt, nil
+}
