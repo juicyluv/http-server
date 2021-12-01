a8bd04d6cc8e9f77a2349e6b907d766c750c6fc0
ellywynn <canttrymybest@gmail.com>
2021-12-01 19:36:07 +0300

    added validation to place structure

--- internal/app/models/place.go
+++ internal/app/models/place.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+)
+
 type Place struct {
 	Id   uint   `json:"id"`
 	Name string `json:"name" binding:"required"`
@@ -7,4 +12,11 @@ type Place struct {
 
 type UpdatePlaceInput struct {
 	Name *string `json:"name"`
+}
+
+func (p *Place) Validate() error {
+	return validation.ValidateStruct(
+		p,
+		validation.Field(&p.Name, is.Alphanumeric, validation.Required),
+	)
 }
