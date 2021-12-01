4928500ea30586bda69e62b1968a8427e37f964c
ellywynn <canttrymybest@gmail.com>
2021-12-01 20:50:47 +0300

    added user role validation

--- internal/app/models/user_role.go
+++ internal/app/models/user_role.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+)
+
 type UserRole struct {
 	Id   uint   `json:"id"`
 	Role string `json:"role"`
@@ -7,4 +12,11 @@ type UserRole struct {
 
 type UpdateUserRoleInput struct {
 	Role *string `json:"role"`
+}
+
+func (u *UserRole) Validate() error {
+	return validation.ValidateStruct(
+		u,
+		validation.Field(&u.Role, is.Alpha),
+	)
 }
