ca929be5dc8755d0b484baae6949d6e026225574
ellywynn <canttrymybest@gmail.com>
2021-12-01 19:28:01 +0300

    implemented places repository

--- internal/app/repository/place.go
+++ internal/app/repository/place.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/ellywynn/http-server/internal/app/models"
 	"github.com/jmoiron/sqlx"
 )
@@ -16,21 +19,58 @@ func NewPlaceRepository(db *sqlx.DB) *PlaceRepository {
 }
 
 func (pr *PlaceRepository) Create(place *models.Place) (uint, error) {
-	return 0, nil
+	var placeId uint
+	query := "INSER INTO palaces (name) VALUES ($1) RETURNING id"
+	if err := pr.db.QueryRow(query, place.Name).Scan(&placeId); err != nil {
+		return 0, err
+	}
+
+	return placeId, nil
 }
 
 func (pr *PlaceRepository) FindAll() (*[]models.Place, error) {
-	return nil, nil
+	var places []models.Place
+	query := "SELECT * FROM places"
+	if err := pr.db.Select(&places, query); err != nil {
+		return nil, err
+	}
+
+	return &places, nil
 }
 
 func (pr *PlaceRepository) FindById(placeId int) (*models.Place, error) {
-	return nil, nil
+	var place models.Place
+	query := "SELECT * FROM places WHERE id = $1"
+	if err := pr.db.Get(&place, query, placeId); err != nil {
+		return nil, err
+	}
+
+	return &place, nil
 }
 
 func (pr *PlaceRepository) Update(placeId int, place *models.UpdatePlaceInput) error {
-	return nil
+	values := make([]string, 0)
+	args := make([]interface{}, 0)
+	argId := 1
+
+	if place.Name != nil {
+		values = append(values, fmt.Sprintf("name=%d", argId))
+		args = append(args, place.Name)
+		argId++
+	}
+
+	valuesQuery := strings.Join(values, ", ")
+	args = append(args, placeId)
+
+	query := fmt.Sprintf("UPDATE places SET %s WHERE id=$%d", valuesQuery, argId)
+	_, err := pr.db.Exec(query, args...)
+
+	return err
 }
 
 func (pr *PlaceRepository) Delete(placeId int) error {
-	return nil
+	query := "DELETE FROM places WHERE id = $1"
+	_, err := pr.db.Exec(query, placeId)
+
+	return err
 }
