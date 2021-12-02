fe3b8be7b251b91b5b2dcb21fbe6a21cadd7481f
ellywynn <canttrymybest@gmail.com>
2021-12-02 18:07:32 +0300

    added test travel instance

--- internal/app/models/testing.go
+++ internal/app/models/testing.go
@@ -1,6 +1,10 @@
 package models
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/lane-c-wagner/go-tinydate"
+)
 
 // Returns default user entity
 func TestUser(t *testing.T) *User {
@@ -8,5 +12,22 @@ func TestUser(t *testing.T) *User {
 		Email:             "user@mail.com",
 		Username:          "admin",
 		EncryptedPassword: "qwerty",
+		Role:              1,
+	}
+}
+
+// Returns default travel entity
+func TestTravel(t *testing.T) *Travel {
+	partySize := 10
+
+	return &Travel{
+		Title:        "Title",
+		DurationDays: 10,
+		Price:        200,
+		PartySize:    &partySize,
+		Complexity:   4,
+		Place:        1,
+		Description:  "desc",
+		Date:         tinydate.Now().ToTime(),
 	}
 }
