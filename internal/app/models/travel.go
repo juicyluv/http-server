5d270a9423be979fe1d5e138c9ddf8efe6119edb
ellywynn <canttrymybest@gmail.com>
2021-12-11 21:15:16 +0300

    added image url field

--- internal/app/models/travel.go
+++ internal/app/models/travel.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"time"
+	"strings"
 
 	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/go-ozzo/ozzo-validation/is"
@@ -16,18 +16,21 @@ type Travel struct {
 	Complexity   int         `json:"complexity" binding:"required"`
 	Place        interface{} `json:"place" binding:"required"`
 	Description  string      `json:"description" binding:"required"`
-	Date         time.Time   `json:"date" binding:"required"`
+	Date         string      `json:"date" binding:"required"`
+	ImageURL     string      `json:"URL" db:"image_url"`
 }
 
 type UpdateTravelInput struct {
-	Title        *string    `json:"title"`
-	DurationDays *int       `json:"duration_days" db:"duration_days"`
-	Price        *int       `json:"price"`
-	PartySize    *int       `json:"party_size" db:"party_size"`
-	Complexity   *int       `json:"complexity"`
-	Place        *int       `json:"place"`
-	Description  *string    `json:"description"`
-	Date         *time.Time `json:"date"`
+	Title        *string `json:"title"`
+	DurationDays *int    `json:"duration_days" db:"duration_days"`
+	Price        *int    `json:"price"`
+	PartySize    *int    `json:"party_size" db:"party_size"`
+	Complexity   *int    `json:"complexity"`
+	Place        *int    `json:"place"`
+	Description  *string `json:"description"`
+	Date         *string `json:"date"`
+	ImageURL     *string `json:"URL" db:"image_url"`
+	Image        *string `json:"image"`
 }
 
 // Validates creating user struct
@@ -42,5 +45,10 @@ func (t *Travel) Validate() error {
 		validation.Field(&t.Description, is.ASCII, validation.Required),
 		validation.Field(&t.Date, validation.Required),
 		validation.Field(&t.Place, validation.Required),
+		validation.Field(&t.ImageURL, validation.Required, is.URL),
 	)
+}
+
+func (t *Travel) FormatDate() {
+	t.Date = strings.Split(t.Date, "T")[0]
 }
