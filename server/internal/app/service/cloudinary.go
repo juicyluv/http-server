35c8f6c77ba062a3e867da45e599fd0b78e324a5
ellywynn <canttrymybest@gmail.com>
2021-12-20 01:41:18 +0300

    cloudinary service for uploading images

---
+++ server/internal/app/service/cloudinary.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/cloudinary/cloudinary-go"
+	"github.com/cloudinary/cloudinary-go/api/uploader"
+	"github.com/ellywynn/http-server/server/internal/app/models/interfaces"
+)
+
+type CloudinaryService struct {
+	cld *cloudinary.Cloudinary
+}
+
+// Returns cloudinary service instance with config from .env file
+// Fatals if cloudinary cannot be created
+func NewCloudinaryService() interfaces.CloudinaryService {
+	cloud := os.Getenv("CLD_CLOUD")
+	key := os.Getenv("CLD_KEY")
+	secret := os.Getenv("CLD_SECRET")
+	cld, err := cloudinary.NewFromParams(cloud, key, secret)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	return &CloudinaryService{
+		cld: cld,
+	}
+}
+
+// Uploads an image and returns image URL or error
+func (c *CloudinaryService) UploadImage(filename, path, cldFolder string) (string, error) {
+	resp, err := c.cld.Upload.Upload(context.TODO(), path, uploader.UploadParams{Folder: cldFolder, Overwrite: true, PublicID: filename})
+	if err != nil {
+		return "", err
+	}
+	return resp.URL, nil
+}
